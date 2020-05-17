6352e49eccfc7647ee8bbeb4acc1cd0f8c24febc
nforesperance <nforesperance1@gmail.com>
2020-05-17 10:18:26 +0100

    bottom nav bar added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:buddiesgram/pages/HomePage.dart';
 import 'package:flutter/material.dart';
 
 void main()
@@ -21,14 +22,7 @@ class MyApp extends StatelessWidget {
         cardColor: Colors.white70,
         accentColor: Colors.black,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Coding Cafe', style: TextStyle(color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold),),
-        ),
-        body: Center(
-          child: Text('Hello World', style: TextStyle(color: Colors.white, fontSize: 30.0,),),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
